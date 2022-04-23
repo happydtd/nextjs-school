c5e03a4c53b8bee74deaa62a355a13db7742bff6
yijun wang <CWang@machealth.com.au>
2022-04-23 14:31:28 +1000

    add course

--- components/CourseCard/index.tsx
+++ components/CourseCard/index.tsx
@@ -1,30 +1,85 @@
 import React from 'react'
-import { Card } from 'antd';
+import { Card ,Row, Col, Divider, Typography, Button} from 'antd';
+import { HeartFilled ,UserOutlined } from '@ant-design/icons';
 import Image from 'next/image';
 
 interface Course{
     name: string
-}
+    createdAt: Date
+    detail:string
+    duration:number
+    id:number
+    maxStudents:number
+    price:number
+    star:number
+    teacherName:string
+  }
+
 interface CourseCardProp {
     course: Course
 }
 
 
 export default function CourseCard({course}:CourseCardProp) {
-
+  const style = { background: '#0092ff', padding: '8px 0' };
   return (
     <>
         <Card title="Default size card" hoverable 
-        cover={
-            <Image
-            alt="Next.js logo"
-            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-            width={300}
-            height={150}
-            />}>
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
+          cover={
+              <Image
+              alt="Next.js logo"
+              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              width={300}
+              height={150}
+              />}>
+            <Row gutter={16}>
+              <Col className="gutter-row" span={24}>
+                <div><Typography>{course.name}</Typography></div>
+              </Col>
+            </Row>
+            <Divider orientation="left"></Divider>
+            <Row gutter={16}>
+              <Col className="gutter-row" span={12}>
+              <div><Typography>{course.createdAt}</Typography></div>
+              </Col>
+              <Col className="gutter-row" span={12}>
+              <div><Typography><HeartFilled style={{color:'red'}}/> {course.star}</Typography></div>
+              </Col>
+            </Row>
+            <Divider orientation="left"></Divider>
+            <Row gutter={16}>
+              <Col className="gutter-row" span={12}>
+              <div><Typography>Duration:</Typography></div>
+              </Col>
+              <Col className="gutter-row" span={12}>
+              <div><Typography>{course.duration} years</Typography></div>
+              </Col>
+            </Row>
+            <Divider orientation="left"></Divider>
+            <Row gutter={16}>
+              <Col className="gutter-row" span={12}>
+              <div><Typography>Teacher:</Typography></div>
+              </Col>
+              <Col className="gutter-row" span={12}>
+              <div><Typography>{course.teacherName}</Typography></div>
+              </Col>
+            </Row>
+            <Divider orientation="left"></Divider>
+            <Row gutter={16}>
+              <Col className="gutter-row" span={12}>
+              <div><Typography><UserOutlined /> Student Limit:</Typography></div>
+              </Col>
+              <Col className="gutter-row" span={12}>
+              <div><Typography>{course.maxStudents}</Typography></div>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col className="gutter-row" span={12}>
+                <Button type="primary">Read More</Button>
+              </Col>
+              <Col className="gutter-row" span={6}/>
+              <Col className="gutter-row" span={6}/>
+            </Row>
         </Card>
     </>
   )
--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext, useState } from 'react';
+import React, { useEffect, useContext, useState, useRef } from 'react';
 import { Row, Col } from 'antd';
 import { useRouter } from 'next/router';
 import 'antd/dist/antd.css';
@@ -16,8 +16,13 @@ export default function Course() {
   const router = useRouter();
   const { userInfo} = state;
   const { token } = userInfo.userInfo;
-  const [ pageSize, setPageSize ] = useState(12);
+  const [ pageSize, setPageSize ] = useState(15);
   const [ courses, setCourses ] = useState(null);
+
+    // set default value
+    const [scrollTop, setScrollTop] = useState(document.body.scrollTop);
+     // create element ref
+  const innerRef = useRef(null);
 
   async function callAPI(){ 
     try{
@@ -37,24 +42,61 @@ export default function Course() {
     callAPI();
   },[pageSize])
 
+  useEffect(() => {
+    const div = innerRef.current;
+    // subscribe event
+    div.addEventListener("scroll", handleOnScroll);
+    return () => {
+      // unsubscribe event
+      div.removeEventListener("scroll", handleOnScroll);
+    };
+  }, []);
+
+  const handleOnScroll = (e) => {
+    // NOTE: This is for the sake of demonstration purpose only.
+    // Doing this will greatly affect performance.
+    setScrollTop(e.target.scrollTop);
+    console.log(e.target.scrollTop);
+  }
 
   return (
     <CommonLayout>
-          <>
-
-            <Row>
+        <div ref={innerRef} style={{overflow: 'scroll',height: 1000}}>
+            <Row gutter={[16, { xs: 8, sm: 16, md: 24, lg: 32 }]}>
               { courses?.map((course, index)=>{
                 console.log(course);
                 return (
-                  <Col key={index} span={6}><CourseCard course={course}/></Col>
+                  <Col key={index} xs={12} sm={12} md={8} lg={6} ><CourseCard course={course}/></Col>
                 )
               })
 
               }
-
             </Row>
-        </>
+        </div>
     </CommonLayout>
+
+    // <>
+    //   {`ScrollTop: ${scrollTop}`}
+    //   <div
+    //     // style={{
+    //     //   // overflow: 'auto',
+    //     //   // width: 500,
+    //     //   // height: 500,
+    //     //   border: '1px solid black',
+    //     // }}
+        
+    //   >
+    //     <div style={{ height: 150, width: 150 , overflow: 'scroll', border: 5 , borderColor: 'orange'}} ref={innerRef}>
+    //       Scroll Me
+    //           <p>Far out in the uncharted backwaters of the unfashionable end
+    //       of the western spiral arm of the Galaxy lies a small unregarded
+    //       yellow sun. Orbiting this at a distance of roughly ninety-two million
+    //       miles is an utterly insignificant little blue green planet whose
+    //       ape-descended life forms are so amazingly primitive that they still
+    //       think digital watches are a pretty neat idea.</p>
+    //     </div>
+    //   </div>
+    // </>
   )
 }
 
