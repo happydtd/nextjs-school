8107e5f26e95158777de8476f2e5234d0a42d945
yijun wang <CWang@machealth.com.au>
2022-04-27 08:54:59 +1000

    udpate course interface

--- models/course.interface.ts
+++ models/course.interface.ts
@@ -1,11 +1,71 @@
-export default interface Course{
+interface Teacher {
+    createdAt: Date,
+    updatedAt: Date,
+    id: number,
+    country: string,
+    courseAmount: number,
+    email: string,
+    name: string,
+    phone: string,
+    profileId: number
+  }
+
+interface Schedule{
+    createdAt: Date,
+    updatedAt: Date,
+    id: number,
+    status: number,
+    current: number,
+    classTime: string[],
+    chapters: Chapter[],
+  }
+
+interface Chapter {
+    createdAt: Date,
+    updatedAt: Date,
+    id: number,
+    name: string,
+    order: number,
+    content: string
+  }
+
+interface type{
+    id: number,
     name: string
-    createdAt: Date
-    detail:string
-    duration:number
-    id:number
-    maxStudents:number
-    price:number
-    star:number
-    teacherName:string
-}
+  }
+
+interface sales{
+    createdAt: Date,
+    updatedAt: Date,
+    id: number,
+    batches: number,
+    price: number,
+    earnings: number,
+    paidAmount: number,
+    studentAmount: number,
+    paidIds: number[]
+  }
+
+  export default interface Course{
+    createdAt: string,
+    updatedAt: Date,
+    id: number,
+    cover: string,
+    detail: string,
+    duration: number,
+    durationUnit: number,
+    maxStudents: number,
+    name: string,
+    price: number,
+    uid: string,
+    star: number,
+    startTime: Date,
+    status: number,
+    scheduleId: number,
+    teacherId: number,
+    teacher: Teacher,
+    schedule: Schedule,
+    type: type[],
+    sales: sales,
+    teacherName: string
+  }
